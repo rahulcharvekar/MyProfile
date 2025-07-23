304888e61c1115878d339cb96502b593100f03e1
Rahul Charvekar <rahulcharvekar@gmail.com>
2025-07-23 10:26:45 +0530

    added env for api endpoint

--- Frontend/my-profile/src/components/Contact.jsx
+++ Frontend/my-profile/src/components/Contact.jsx
@@ -1,3 +1,21 @@
-export default function Contact() {
-  return <div>Contact Information</div>;
+import React, { useState, useEffect } from 'react';
+
+function Contact() {
+  const [message, setMessage] = useState('');
+  const apiUrl = import.meta.env.VITE_API_URL;
+
+  useEffect(() => {
+    fetch(apiUrl)
+      .then(response => response.json())
+      .then(data => setMessage(data.message))
+      .catch(error => console.error('Error:', error));
+  }, []);
+
+  return (
+    <div>
+      {message && <p>Python API says: {message}</p>}
+    </div>
+  );
 }
+
+export default Contact;
